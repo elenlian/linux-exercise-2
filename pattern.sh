04c875ef674703d82f5f91cce75eacdc6074bb66
Eleni Lianoudaki <eleni.lianoudaki@hotmail.com>
2018-11-26 20:32:41 +0200

    add pattern.sh

--- pattern.sh
+++ pattern.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
+#Download the fasta file.
 wget http://139.91.162.50/bioinf2018/fasta.fa
-grep -ie '-[^0-9]' fasta.fa | wc -l
-grep -- --  fasta.fa | wc -l
-grep -ie '[acgt]-[acgt]' fasta.fa | wc -l
-egrep -ix '[acgt]+' fasta.fa | wc -l
-grep -i 'GGG[CT][AG]' fasta.fa | wc -l
 
-#export only the sequence names and find out how many sequences are from human (hg19)
+#Count how many sequences have at least an indel (i.e. at least a single ‘-’)
+grep -ie '-[^0-9]' fasta.fa | wc -l 
+##2607 sequences
+
+#Count how many sequences have at least two continuous indels (i.e. ‘–’)
+grep -- --  fasta.fa | wc -l
+##2491 sequences
+
+#Count how many sequences have a pattern that looks like that N_N_N, where N can be any nucleotide.
+grep -ie '[acgt]-[acgt]' fasta.fa | wc -l
+##504 sequences
+
+#Count how many sequences have no '-'
+egrep -ix '[acgt]+' fasta.fa | wc -l
+##2392 sequences
+
+#Extract all motifs that are three ’G’s then a pyrimidine and then a purine.
+grep -i 'GGG[CT][AG]' fasta.fa | wc -l
+##1213 sequences
+
+#Export only the sequence names and find out how many sequences are from human (hg19)
 grep '^>' fasta.fa > sequences.txt
 grep "hg19" sequences.txt | wc -l
 ##50 human sequences
